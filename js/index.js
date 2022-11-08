4205e45219cccbe0cf3c896fb95dea7a58f4b788
Maxim Nekrasov <maxis1507@gmail.com>
2022-11-08 21:41:11 +0300

    mod: player

--- js/index.js
+++ js/index.js
@@ -3,84 +3,84 @@ const dataMusic = [
       id: '1',
       artist: 'The weekend',
       track: 'Save your tears',
-      poster: 'img/photo1.jpg',
-      mp3: 'audio/The Weeknd - Save Your Tears.mp3',
+      poster: 'img/Photo1.jpg',
+      mp3: 'audio/The Weekend - Save Your Tears.mp3',
     },
     {
       id: '2',
       artist: 'Imagine Dragons',
       track: 'Follow You',
-      poster: 'img/photo2.jpg',
+      poster: 'img/Photo2.jpg',
       mp3: 'audio/Imagine Dragons - Follow You.mp3',
     },
     {
       id: '3',
       artist: 'Tove Lo',
       track: 'How Long',
-      poster: 'img/photo3.jpg',
+      poster: 'img/Photo3.jpg',
       mp3: 'audio/Tove Lo - How Long.mp3',
     },
     {
       id: '4',
       artist: 'Tom Odell',
       track: 'Another Love',
-      poster: 'img/photo4.jpg',
+      poster: 'img/Photo4.jpg',
       mp3: 'audio/Tom Odell - Another Love.mp3',
     },
     {
       id: '5',
       artist: 'Lana Del Rey',
       track: 'Born To Die',
-      poster: 'img/photo5.jpg',
+      poster: 'img/Photo5.jpg',
       mp3: 'audio/Lana Del Rey - Born To Die.mp3',
     },
     {
       id: '6',
       artist: 'Adele',
       track: 'Hello',
-      poster: 'img/photo6.jpg',
+      poster: 'img/Photo6.jpg',
       mp3: 'audio/Adele - Hello.mp3',
     },
     {
       id: '7',
       artist: 'Tom Odell',
       track: "Can't Pretend",
-      poster: 'img/photo7.jpg',
+      poster: 'img/Photo7.jpg',
       mp3: "audio/Tom Odell - Can't Pretend.mp3",
     },
     {
       id: '8',
       artist: 'Lana Del Rey',
       track: 'Young And Beautiful',
-      poster: 'img/photo8.jpg',
+      poster: 'img/Photo8.jpg',
       mp3: 'audio/Lana Del Rey - Young And Beautiful.mp3',
     },
     {
       id: '9',
       artist: 'Adele',
       track: 'Someone Like You',
-      poster: 'img/photo9.jpg',
+      poster: 'img/Photo9.jpg',
       mp3: 'audio/Adele - Someone Like You.mp3',
     },
     {
       id: '10',
       artist: 'Imagine Dragons',
       track: 'Natural',
-      poster: 'img/photo10.jpg',
+      poster: 'img/Photo10.jpg',
       mp3: 'audio/Imagine Dragons - Natural.mp3',
     },
     {
       id: '11',
       artist: 'Drake',
       track: 'Laugh Now Cry Later',
-      poster: 'img/photo11.jpg',
+      poster: 'img/Photo11.jpg',
       mp3: 'audio/Drake - Laugh Now Cry Later.mp3',
     },
     {
       id: '12',
       artist: 'Madonna',
       track: 'Frozen',
-      poster: 'img/photo12.jpg',
+      poster: 'img/Photo12.jpg',
       mp3: 'audio/Madonna - Frozen.mp3',
     },
   ];
@@ -102,14 +102,14 @@ const playMusic = (event) => {
 
     for (let i = 0; i < tracksCard.length; i++) {
         tracksCard[i].classList.remove('track_active');
-    }
+    };
 
     trackActive.classList.add('track_active');
 };
 
 for (let i = 0; i < tracksCard.length; i++) {
     tracksCard[i].addEventListener('click', playMusic);
-}
+};
 
 pauseBtn.addEventListener('click', () => {
     if (audio.paused) {
@@ -124,13 +124,32 @@ pauseBtn.addEventListener('click', () => {
 stopBtn.addEventListener('click', () => {
     player.classList.remove('player_active');
     audio.src = '';
-})
+});
+
+const createCard = (data) => {
+  const card = document.createElement('a');
+  card.classList.add('catalog__item', 'track');
+  card.dataset.idTrack = data.id;
+
+  card.innerHTML = `
+    <div class="track__img-wrap">
+      <img src="${data.poster}" alt="${data.artist} ${data.track}" class="track__poster" width="180" height="180">
+    </div>
+    <div class="track__info track-info">
+      <p class="track-info__title">${data.track}</p>
+      <p class="track-info__artist">${data.artist}</p>
+    </div>
+  `;
+
+  return card
+};
 
 const renderCatalog = (dataList) => {
-    catalogContainer.textContent = '',
-    const listCards = dataList.map(() => {})
-    console.log('listCards: ', listCards);
-}
+    catalogContainer.textContent = '';
+    const listCards = dataList.map(createCard);
+    catalogContainer.append(...listCards);
+};
+
 
 const init = () => {
     renderCatalog(dataMusic);
